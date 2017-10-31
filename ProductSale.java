0a80bc8efd783ebed50b99eaf1e8855f6cc7be87
limo-756 <anuragsharma05031996@gmail.com>
2017-10-31 16:50:57 +0530

    Adding new class ProductSale to get the sale report of each Product

---
+++ ProductSale.java
@@ -0,0 +1,99 @@
+import java.util.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.BasicDBObject;
+import com.mongodb.ServerAddress;
+import com.mongodb.DBCursor;
+import java.util.Iterator;
+
+public class ProductSale
+{
+    ObjectId id;
+    String name;
+    float rate;
+    float qty;
+    float taxSlab;
+    float totalTax;
+    float discRate;
+    float totalDiscount;
+    float totalPrice;
+    ProductSale(ObjectId id,String name,float rate,float qty,float taxSlab,float taxSlab,float totalTax,float discRate,
+        float totalDiscount,float totalPrice)
+    {
+        this.id = id;
+        this.name = name;
+        this.rate = rate;
+        this.qty = qty;
+        this.taxSlab = taxSlab;
+        this.totalTax = totalTax;
+        this.discRate = discRate;
+        this.totalDiscount = totalDiscount;
+        this.totalPrice = totalPrice;
+    }
+    ProductSale(Utility obj)
+    {
+        id = (ObjectId) obj.getAttribute("transactionId");
+        name = obj.getAttribute("name");
+        rate = Float.valueOf(obj.getAttribute("rate"));
+        qty = Float.valueOf(obj.getAttribute("qty"));
+        taxSlab = Float.valueOf(obj.getAttribute("taxSlab"));
+        totalTax = Float.valueOf(obj.getAttribute("totalTax"));
+        discRate = Float.valueOf(obj.getAttribute("discRate"));
+        totalDiscount = Float.valueOf(obj.getAttribute("totalDiscount"));
+        totalPrice = Float.valueOf(obj.getAttribute("totalPrice"));
+    }
+    void addNewProductSale()
+    {
+        DatabaseConnect db = new DatabaseConnect("ProductSale");
+        Document document = new Document("transactionId",id)
+        .append("name",name)
+        .append("rate",rate)
+        .append("qty",qty)
+        .append("taxSlab",taxSlab)
+        .append("totalTax",totalTax)
+        .append("discRate",discRate);
+        .append("totalDiscount",totalDiscount);
+        .append("totalPrice",totalPrice);
+        db.getCollection().insertOne(document);
+    }
+    ObjectId getTransactionId()
+    {
+        return id;
+    }
+    String getName()
+    {
+        return name;
+    }
+    float getRate()
+    {
+        return rate;
+    }
+    float getQuantity()
+    {
+        return qty;
+    }
+    float getTaxSlab()
+    {
+        return taxSlab;
+    }
+    float getTotalTax()
+    {
+        return totalTax;
+    }
+    float getDiscountRate()
+    {
+        return discRate;
+    }
+    float getTotalDiscount()
+    {
+        return totalDiscount;
+    }
+    float getTotalPrice()
+    {
+        return totalPrice;
+    }
+}
