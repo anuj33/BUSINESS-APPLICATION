5f27702d572bd6717e860d2e46e00f5de0661682
limo-756 <anuragsharma05031996@gmail.com>
2017-11-02 01:42:26 +0530

    Add functionality for getting monthly sales total sale record

--- SalesReport.java
+++ SalesReport.java
@@ -33,31 +33,162 @@ public class SalesReport
     int type;
     Date startDate;
     Date endDate;
+    Vector<Product> product;
+    Vector<Double> transactionAmount;
+    Vector<String> xAxis = new Vector<String>();
+    Vector<String> yAxis = new Vector<String>();
+    Vector<Date> startBucket = new Vector<Date>();
+    Vector<Date> endBucket = new Vector<Date>();
+    SalesReport(int firmId,int type,Date startDate,Date endDate,Vector<Product> product)
+    {
+        this.firmId = firmId;
+        this.type = type;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.product = product;
+    }
     SalesReport(int firmId,int type,Date startDate,Date endDate)
     {
         this.firmId = firmId;
         this.type = type;
         this.startDate = startDate;
         this.endDate = endDate;
+        this.product = product;
     }
     Date getStartOfDay(Date date)
     {
-        Utility dateExtractor = Utility();
+        Utility dateExtractor = new Utility();
+        dateExtractor.dateHandeler(date.toString());
         int year = Integer.parseInt(dateExtractor.getAttribute("year"));
         int month = Utility.getMonth(dateExtractor.getAttribute("month"));
-        int date = Integer.parseInt(dateExtractor.getAttribute("date"));
+        int currDate = Integer.parseInt(dateExtractor.getAttribute("date"));
         Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, day, 0, 0, 0);
+        calendar.set(year, month, currDate, 0, 0, 0);
         return calendar.getTime();
     }
     Date getEndOfDay(Date date)
     {
-        Utility dateExtractor = Utility();
+        Utility dateExtractor = new Utility();
+        dateExtractor.dateHandeler(date.toString());
         int year = Integer.parseInt(dateExtractor.getAttribute("year"));
         int month = Utility.getMonth(dateExtractor.getAttribute("month"));
-        int date = Integer.parseInt(dateExtractor.getAttribute("date"));
+        int currDate = Integer.parseInt(dateExtractor.getAttribute("date"));
         Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, day, 23, 59, 59);
+        calendar.set(year, month, currDate, 23, 59, 59);
         return calendar.getTime();
     }
+    Date getStartOfMonth(Date date)
+    {
+        Utility dateExtractor = new Utility();
+        dateExtractor.dateHandeler(date.toString());
+        int year = Integer.parseInt(dateExtractor.getAttribute("year"));
+        int month = Utility.getMonth(dateExtractor.getAttribute("month"));
+        int currDate = 1;
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, currDate, 0, 0, 0);
+        return calendar.getTime();
+    }
+    Date getEndOfMonth(Date date)
+    {
+        Utility dateExtractor = new Utility();
+        dateExtractor.dateHandeler(date.toString());
+        int year = Integer.parseInt(dateExtractor.getAttribute("year"));
+        int month = Utility.getMonth(dateExtractor.getAttribute("month"));
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, Utility.getNumberOfDays(month,year), 23, 59, 59);
+        return calendar.getTime();
+    }
+    Date decrementMonth(Date date)
+    {
+        Utility dateExtractor = new Utility();
+        dateExtractor.dateHandeler(date.toString());
+        int year = Integer.parseInt(dateExtractor.getAttribute("year"));
+        int month = Utility.getMonth(dateExtractor.getAttribute("month"));
+        int currDate = Integer.parseInt(dateExtractor.getAttribute("date"));
+        int hour = Integer.parseInt(dateExtractor.getAttribute("hour"));
+        int minute = Integer.parseInt(dateExtractor.getAttribute("minute"));
+        int second = Integer.parseInt(dateExtractor.getAttribute("second"));
+        if(month == 1)
+        {
+            year--;
+            month = 12;
+        }
+        else
+            month--;
+        if(currDate > Utility.getNumberOfDays(month, year))
+            currDate = Utility.getNumberOfDays(month, year);
+        System.out.println("IN month "+month);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, currDate, hour, minute, second);
+        System.out.println("IN Calendar"+calendar.getTime());
+        return calendar.getTime();
+    }
+    void createReport()
+    {
+        System.out.println("start date end date"+startDate + " " + endDate);
+        BusinessFirm firm = new BusinessFirm();
+        firm.fetchTransaction(firmId);
+        Vector<Transaction> record = firm.getTransactionList();
+        int numOfRecords = firm.getTransactionNumber();
+        Vector<Transaction> recordWithinRange = new Vector<Transaction>();
+        System.out.println("record size "+ record.size() + "  "+numOfRecords);
+        for (int i = 0;i < record.size() ;i++ )
+        {
+            System.out.println(record.get(i).getBillingDate());
+            if(record.get(i).getBillingDate().after(startDate) && record.get(i).getBillingDate().before(endDate))
+                recordWithinRange.add(record.get(i));
+        }
+        Vector<Double> bucket = new Vector<Double>();
+        Date date = new Date();
+        date.setTime(endDate.getTime());
+        while(date.after(startDate))
+        {
+            Date a = getStartOfMonth(date);
+            Date b = getEndOfMonth(date);
+            startBucket.add(a);
+            endBucket.add(b);
+            date = decrementMonth(date);
+            System.out.println("In while LOOP" + date);
+        }
+        Date a = getStartOfMonth(date);
+        Date b = getEndOfMonth(date);
+        startBucket.add(a);
+        endBucket.add(b);
+        if(endBucket.get(0).after(endDate))
+            endBucket.set(0,endDate);
+        if(startBucket.get(startBucket.size() - 1).before(startDate))
+            startBucket.set(startBucket.size() - 1,startDate);
+        Collections.reverse(startBucket);
+        Collections.reverse(endBucket);
+        for (int i = 0;i < startBucket.size() ; i++ )
+            bucket.add(0.0);
+        System.out.println("Size of "+ recordWithinRange.size());
+        System.out.println("Size of "+ startBucket.size());
+        System.out.println("start date end date"+startDate + " " + endDate);
+        for (int i = 0;i < recordWithinRange.size() ; i++ )
+        {
+            for(int j = 0;j < startBucket.size();j++)
+            {
+                System.out.println(startBucket.get(j)+ " " + endBucket.get(j));
+                if(recordWithinRange.get(i).getBillingDate().after(startBucket.get(j)) &&
+                    recordWithinRange.get(i).getBillingDate().before(endBucket.get(j)))
+                {
+                System.out.println(recordWithinRange.get(i)+ " " + startBucket.get(j));
+                    bucket.set(j,bucket.get(j) + recordWithinRange.get(i).getTotalAmt());
+                }
+            }
+        }
+        transactionAmount = bucket;
+    }
+    Vector<Double> getTransactionAmount()
+    {
+        return transactionAmount;
+    }
+    // Vector<String> getXAxis()
+    // {
+    //     for(int i = 0;i < transactionAmount.size();i++)
+    //     {
+    //         xAxis.add();
+    //     }
+    // }
 }
