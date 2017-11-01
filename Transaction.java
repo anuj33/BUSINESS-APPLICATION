f2a0d4d465cd218ed33a1e2f82afa720fd30711f
limo-756 <anuragsharma05031996@gmail.com>
2017-11-01 16:26:34 +0530

    Corrected the Date Format string and removed a duplicate parameter from constructor

--- Transaction.java
+++ Transaction.java
@@ -1,16 +1,39 @@
 import java.util.*;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+import com.mongodb.util.JSON;
+import java.io.IOException;
+import java.util.*;
+import java.util.Iterator;
+import org.bson.Document;
+import java.lang.Object;
+import org.bson.types.ObjectId;
+
 public class Transaction
 {
     int firmId;
-    String invoiceNumber;
+    int invoiceNumber;
     Date billingDate;
     float totalAmt;
     float totalDiscount;
     float sgst,cgst,igst;
     ObjectId id;
-    Transaction(int firmId,String invoiceNumber,Date billingDate,float totalAmt,float totalDiscount,
+    String transactionId;
+    public Transaction(int firmId,int invoiceNumber,Date billingDate,float totalAmt,float totalDiscount,
         float sgst,float cgst,float igst)
     {
         this.firmId = firmId;
@@ -22,15 +45,17 @@ public class Transaction
         this.cgst = cgst;
         this.igst = igst;
     }
-    Transaction(Utility obj)
+    public Transaction(Utility obj)
     {
         firmId = Integer.parseInt(obj.getAttribute("firmId"));
-        invoiceNumber = obj.getAttribute("invoiceNumber");
+        invoiceNumber = Integer.parseInt(obj.getAttribute("invoiceNumber"));
         String temp = obj.getAttribute("billingDate");
+        System.out.println(temp);
         try
         {
-            SimpleDateFormat ft = new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+            SimpleDateFormat ft = new SimpleDateFormat ("EEE MMM dd HH:mm:ss zzz yyyy");
             billingDate = ft.parse(temp);
+            System.out.println(billingDate);
         }
         catch(Exception e){}
         totalAmt = Float.valueOf(obj.getAttribute("totalAmt"));
@@ -38,26 +63,34 @@ public class Transaction
         sgst = Float.valueOf(obj.getAttribute("sgst"));
         cgst = Float.valueOf(obj.getAttribute("cgst"));
         igst = Float.valueOf(obj.getAttribute("igst"));
+        transactionId = obj.getAttribute("_id");
     }
     void addNewTransaction()
     {
         DatabaseConnect db = new DatabaseConnect("transaction");
-        Document document = new Document("firmId",newRecord.getFirmId())
-        .append("invoiceNumber",newRecord.getInvoiceNumber())
-        .append("billingDate",newRecord.getBillingDate())
-        .append("totalAmt",newRecord.getTotalAmt())
-        .append("totalDiscount",newRecord.getTotalDiscount())
-        .append("sgst",newRecord.getSgst())
-        .append("cgst",newRecord.getCgst())
-        .append("igst",newRecord.getIgst());
+        Document document = new Document("firmId",firmId)
+        .append("invoiceNumber",invoiceNumber)
+        .append("billingDate",billingDate)
+        .append("totalAmt",totalAmt)
+        .append("totalDiscount",totalDiscount)
+        .append("sgst",sgst)
+        .append("cgst",cgst)
+        .append("igst",igst);
         db.getCollection().insertOne(document);
-        ObjectId id = (ObjectId)document.get( "_id" );
+        ObjectId id = (ObjectId)document.get("_id");
+        // DBObject a = (DBObject)document.get("_id");
+        // String reportName = (String) a.get("oid");
+        // System.out.println("Report " + reportName);
+        transactionId = id.toHexString();
+        System.out.println("Transaction working");
+        System.out.println(transactionId);
+        System.out.println(id.toHexString());
     }
     int getFirmId()
     {
         return firmId;
     }
-    String getInvoiceNumber()
+    int getInvoiceNumber()
     {
         return invoiceNumber;
     }
@@ -85,8 +118,8 @@ public class Transaction
     {
         return igst;
     }
-    ObjectId getTransactionId()
+    String getTransactionId()
     {
-        return id;
+        return transactionId;
     }
 }
